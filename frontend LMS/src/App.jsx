776c61fef4483ba34e24e29c8dd398a132f4b5f1
Vikas singh varma <Vikashverma3001@gmail.com>
2024-02-07 16:28:11 +0530

    footer component and not found denied page added

--- frontend LMS/src/App.jsx
+++ frontend LMS/src/App.jsx
@@ -2,12 +2,20 @@ import './App.css'
 import { Routes, Route } from "react-router-dom";
 // import pages
 import Signup from './pages/Signup';
+import HomePage from './pages/HomePage';
+import Aboutus from './pages/Aboutus';
+import Denied from './pages/Denied';
+import Notfound from './pages/NotFound';
 function App() {
 
   return (
     <>
       <Routes >
         <Route path='/signup' element ={<Signup />} />
+        {/* <Route path='/' element={<HomePage />} /> */}
+       {/* <Route path='/about' element={<Aboutus />} /> */}
+       <Route path='/denied' element={<Denied />} />
+      <Route path='*' element={<Notfound />} />
       </Routes>
     </>
   )
