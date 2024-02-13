4f46346a8a022384973ad9a62a92e8bd011e87f2
Vikas singh varma <Vikashverma3001@gmail.com>
2024-02-13 15:06:28 +0530

    lecture slice

--- frontend LMS/src/Redux/Slices/lectureSlice.js
+++ frontend LMS/src/Redux/Slices/lectureSlice.js
@@ -10,6 +10,9 @@ const lectureSlice = createSlice({
     name: "lecture",
     initialState,
     reducers: {},
+    extraReducers: ()=>{
+        
+    }
 })
 
 export default lectureSlice.reducer 
