bce53a509c97d58173b0aaecb769c59b2ec26993
Vikas singh varma <Vikashverma3001@gmail.com>
2025-02-05 12:19:26 +0530

    Refactor: login improve some more field added

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,90 +1,165 @@
 import { Schema, model } from "mongoose";
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import crypto from 'crypto';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     fullName: {
-        type: String,
-        required: [true, 'Name is required'],
-        minLength: [3, 'Name must be at least 5 character'],
-        maxLength: [20, 'Name should be less than 20 character'],
-        lowercase: true,
-        trim: true
+      type: String,
+      required: [true, "Name is required"],
+      minLength: [3, "Name must be at least 4 character"],
+      maxLength: [20, "Name should be less than 20 character"],
+      lowercase: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: [true, 'email is required'],
-        lowercase: true,
-        trim: true,
-        unique: true
+      type: String,
+      required: [true, "Email is required"],
+      lowercase: true,
+      trim: true,
+      unique: true,
+      match: [/.+\@.+\..+/, "Please enter a valid email"],
     },
     password: {
-        type: String,
-        required: [true, 'Password is required'],
-        minLength: [4, 'Password must be at least 4 character'],
-        // select: false
+      type: String,
+      required: [true, "Password is required"],
+      match: [
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/,
+        "Must be 6 chars with at least one uppercase, one lowercase, and one number",
+      ],
+      select: false,
     },
     avatar: {
-        public_id: {
-            type: String
-        },
-        secure_url: {
-            type: String
-        }
+      public_id: {
+        type: String,
+      },
+      secure_url: {
+        type: String,
+      },
+    },
+    bio: {
+      type: String,
+      maxLength: [200, "Bio cannot exceed 200 characters"],
     },
     role: {
-        type: String,
-        default: 'USER',
-        enum: ['USER', 'ADMIN']
+      type: String,
+      enum: {
+        values: ["student", "instructor", "admin"],
+        message: "Please select a valid role",
+      },
+      default: "student",
     },
+    enrolledCourses: [
+      {
+        course: {
+          type: mongoose.Schema.ObjectId,
+          ref: "Course",
+        },
+        enrolledAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    createdCourses: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Course",
+      },
+    ],
     forgotPasswordToken: {
-        type: String,
+      type: String,
     },
     forgotPasswordExpiry: {
-        type: Date,
+      type: Date,
     },
-    subscription: {
-        id: String,
-        status: String
-    }
-},
-    {
-        timestamps: true
-    });
+    lastActive: {
+      type: Date,
+      default: Date.now,
+    },
+    // subscription: {
+    //   id: String,
+    //   status: String,
+    // },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        return next();
-    }
-    this.password = await bcrypt.hash(this.password, 10);
-    return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
+// Compare user password
+userSchema.methods.comparePassword = async function (givedPassword) {
+  return await bcrypt.compare(givedPassword, this.password);
+};
+
+// Generate Reset password Token
+userSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.forgotPasswordExpiry = Date.now() + 10 * (60 * 1000);
+
+  return resetToken;
+};
+
+// virtual field for Total Enrolled courses
+userSchema.virtual("TotalEnrolledCourses").get(function () {
+  return this.enrolledCourses.length;
+});
+
+// virtual field for Total Created courses
+userSchema.virtual("TotalCreatedCourses").get(function () {
+  return this.createdCourses.length;
+});
+
+// last active
+userSchema.methods.updateLastActive = async function () {
+  this.lastActive = Date.now();
+  return await this.lastActive({
+    validateBeforeSave: false,
+  });
+};
+
 userSchema.methods = {
-    generateJWTToken: function () {
-        return jwt.sign(
-            { id: this._id, email: this.email, role: this.role , subscription: this.subscription},
-            process.env.JWT_SECRET,
-            { expiresIn: process.env.JWT_EXPIRY }
-        )
-    },
+  generateJWTToken: function () {
+    return jwt.sign(
+      {
+        id: this._id,
+        email: this.email,
+        role: this.role,
+        subscription: this.subscription,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRY }
+    );
+  },
 
-    generatePasswordResetToken: async function () {
-        const resetToken = await crypto.randomBytes(20).toString('hex');
+  generatePasswordResetToken: async function () {
+    const resetToken = await crypto.randomBytes(20).toString("hex");
 
-        this.forgotPasswordToken = await crypto
-            .createHash('sha256')  
-            .update(resetToken)
-            .digest('hex');
+    this.forgotPasswordToken = await crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
 
-        this.forgotPasswordExpiry = Date.now() + 15 * 60 * 1000; // 15 min from now
+    this.forgotPasswordExpiry = Date.now() + 15 * 60 * 1000; // 15 min from now
 
-        return resetToken;
-    }
-
-}
-
+    return resetToken;
+  },
+};
 
 export default model("User", userSchema);
