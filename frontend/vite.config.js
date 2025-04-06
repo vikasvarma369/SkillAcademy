558777b021318b00e205cc70ef02607026bc2fb7
Vikas singh varma <Vikashverma3001@gmail.com>
2025-04-06 17:38:06 +0530

    fix: remove commented-out proxy configuration in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server:{
-  //   proxy:{
-  //     '/api/v1': import.meta.env.VITE_BASE_URL
-  //   }
-  // },
   plugins: [react()],
 })
