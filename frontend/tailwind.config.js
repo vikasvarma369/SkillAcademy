e8b1ee6eb66689a540d73cdde2d8e644d93b52e9
Vikas singh varma <Vikashverma3001@gmail.com>
2025-04-06 17:37:41 +0530

    fix: add custom font families to Tailwind CSS configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'lato': ['Lato', 'sans-serif'],
+        'nunito-sans': ['Nunito Sans', 'sans-serif'],
+        'open-sans': ['Open Sans', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif'],
+      }
+    },
   },
-  plugins: [require("daisyui")],
+  darkMode: 'class',
+  plugins: [require('daisyui')],
 }
-
