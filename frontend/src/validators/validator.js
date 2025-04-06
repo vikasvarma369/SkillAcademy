a935f49341f029c6787325b2baa149354c8ee96a
Vikas singh varma <Vikashverma3001@gmail.com>
2025-04-06 18:00:35 +0530

    fix: remove unused validator functions for code cleanup

--- frontend/src/validators/validator.js
+++
@@ -1,8 +0,0 @@
-export function isEmailValid(string){
-    return string.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-}
-
-export function isPasswordValid(string){
-    return string.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/);
-}
-
