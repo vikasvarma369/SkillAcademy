bfb8e936b321aa51c6728424bd2a1c0fab17f312
Vikas singh varma <Vikashverma3001@gmail.com>
2025-04-07 11:17:41 +0530

    feat: Refactor Footer and HomePage components for improved layout and styling

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,34 +1,40 @@
+import React from "react";
 import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
 
-function Footer() {
-
-    const newDate = new Date();
-    const year = newDate.getFullYear();
-
-    return (
-
-        <footer className="relative sm:left-0 sm:bottom-0 sm:h-[10vh] gap-5 py-5 flex flex-col sm:flex-row items-center justify-between sm:px-20 text-white bg-gray-800">
-            <section className="text-lg sm:text-center">
-                Copyright {year} | All rights reserved
-            </section>
-
-            <section className="flex items-center justify-center gap-5 text-2xl text-white">
-                <a href="#" className="hover:text-yellow-500 transition-all ease-in-out duration-300">
-                    <BsFacebook />
-                </a>
-                <a href="#" className="hover:text-yellow-500 transition-all ease-in-out duration-300">
-                    <BsInstagram />
-                </a>
-                <a href="#" className="hover:text-yellow-500 transition-all ease-in-out duration-300">
-                    <BsLinkedin />
-                </a>
-                <a href="#" className="hover:text-yellow-500 transition-all ease-in-out duration-300">
-                    <BsTwitter />
-                </a>
-            </section>
-        </footer>
-
-    );
+export default function Footer() {
+  const curDate = new Date();
+  const year = curDate.getFullYear();
+  return (
+    <footer className="py-12 md:px-16 px-3 bg-slate-100 dark:bg-gray-900 flex md:flex-row flex-col md:justify-between justify-center items-center gap-4">
+      <span className="md:text-xl  text-lg font-[600] text-gray-700 dark:text-white">
+        Copyright {year} | All Right Reserved
+      </span>
+      <div className="flex gap-5 items-center">
+        <a
+          href="#"
+          className="md:text-3xl text-xl text-gray-900 dark:text-slate-50 hover:text-gray-500 dark:hover:text-slate-300"
+        >
+          <BsFacebook />
+        </a>
+        <a
+          href="#"
+          className="md:text-3xl text-xl text-gray-900 dark:text-slate-50 hover:text-gray-500 dark:hover:text-slate-300"
+        >
+          <BsInstagram />
+        </a>
+        <a
+          href="#"
+          className="md:text-3xl text-xl text-gray-900 dark:text-slate-50 hover:text-gray-500 dark:hover:text-slate-300"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="#"
+          className="md:text-3xl text-xl text-gray-900 dark:text-slate-50 hover:text-gray-500 dark:hover:text-slate-300"
+        >
+          <BsTwitter />
+        </a>
+      </div>
+    </footer>
+  );
 }
-
-export default Footer;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import HomeLayout from "../layouts/HomeLayout";
-import homePageMainImage from '../assets/Images/homePageMainImage.png';
+import Layout from "../Layout/Layout";
+import heroPng from "../assets/images/hero.png";
 
 export default function HomePage() {
   return (
-    <HomeLayout>
+    <Layout>
       <section className="md:py-10 py-7 mb-10 text-white flex md:flex-row flex-col-reverse items-center justify-center md:gap-10 gap-7 md:px-16 px-6 min-h-[85vh]">
         <div className="md:w-1/2 w-full space-y-7">
           <h1 className="md:text-5xl text-6xl font-semibold text-gray-900 dark:text-gray-200">
@@ -19,13 +19,13 @@ export default function HomePage() {
 
           <div className="space-x-6 flex">
             <Link to="/courses">
-              <button className="bg-yellow-500 hover:border-white hover:border hover:text-yellow-500 hover:bg-transparent  font-semibold text-gray-800 md:px-5 px-3 md:py-3 py-3 rounded-md  md:text-lg cursor-pointer transition-all ease-in-out duration-300">
+              <button className="bg-yellow-500 font-inter font-[400] text-slate-100 dark:text-gray-950 md:px-5 px-3 md:py-3 py-3 rounded-md  md:text-lg text-base cursor-pointer transition-all ease-in-out duration-300">
                 Explore courses
               </button>
             </Link>
 
             <Link to="/contact">
-              <button className="border border-yellow-500 hover:text-yellow-500 hover:border-white text-gray-700 dark:text-white px-5 py-3 rounded-md font-semibold md:text-lg text-base cursor-pointer  transition-all ease-in-out duration-300">
+              <button className="border border-yellow-500 text-gray-700 dark:text-white px-5 py-3 rounded-md font-semibold md:text-lg text-base cursor-pointer  transition-all ease-in-out duration-300">
                 Contact Us
               </button>
             </Link>
@@ -33,9 +33,9 @@ export default function HomePage() {
         </div>
 
         <div className="md:w-1/2 w-1/7 flex items-center justify-center">
-          <img alt="homepage image" src={homePageMainImage} />
+          <img alt="homepage image" src={heroPng} />
         </div>
       </section>
-    </HomeLayout>
+    </Layout>
   );
 }
