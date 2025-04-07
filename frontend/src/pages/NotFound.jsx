61589581ad71c3ed67387f32e675df6c3ab5d856
Vikas singh varma <Vikashverma3001@gmail.com>
2025-04-07 11:19:39 +0530

    feat: Update Denied and NotFound components with improved layout, styling, and image handling

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,20 +1,34 @@
 import { useNavigate } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import NotFoundImg from "../assets/images/not-found.png";
 
-function Notfound() {
-    const navigate = useNavigate();
-    return (
-        <div className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238] min-h-screen pb-8  md:flex-row  md:justify-around dark:bg-[#131212]">
-            <h1 className="text-9xl font-extrabold text-white">404</h1>
-            <div className="bg-black text-white absolute px-2 text-sm rounded rotate-12">
-                Page not found...
-            </div>
-            <button className="mt-5">
-                <a onClick={() => navigate(-1)} className="relative inline-block text-sm font-medium text-[#FF6A3D] active:text-yellow-500 focus:outline-non">
-                    <span  className="relative block px-8 py-3 bg-[#1A2238] border border-current">Go back</span>
-                </a>
-            </button>
+function NotFound() {
+  const navigate = useNavigate();
+  return (
+    <Layout hideBar={true} hideNav={true}>
+      <section className="min-h-screen pb-8 w-full flex md:flex-row flex-col-reverse md:justify-around bg-white  dark:bg-[#131212]">
+        <div className=" flex flex-col justify-center items-center">
+          <h1 className="text-9xl font-extrabold text-gray-700 dark:text-white tracking-widest">
+            404
+          </h1>
+          <div className="bg-red-500 dark:bg-black text-white px-2 text-sm rounded rotate-12 absolute">
+            Page not found ...
+          </div>
+          <button className="mt-5">
+            <a className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-yellow-500 focus:outline-none focus:ring">
+              <span
+                onClick={() => navigate(-1)}
+                className="relative block px-8 py-3 bg-[#1A2238] text-white border border-current"
+              >
+                Go Back
+              </span>
+            </a>
+          </button>
         </div>
-    );
+        <img src={NotFoundImg} alt="" />
+      </section>
+    </Layout>
+  );
 }
 
-export default Notfound;
+export default NotFound;
