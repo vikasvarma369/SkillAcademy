9de264e85082da60657dc5f1f7f76687f0ad0785
Vikas singh varma <Vikashverma3001@gmail.com>
2025-06-05 20:42:52 +0530

    fix: Correct import path for Redux store to use lowercase

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import {Toaster} from 'react-hot-toast';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./Redux/store.js";
+import store from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
