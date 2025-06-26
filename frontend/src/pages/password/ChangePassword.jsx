2f6f977cce9147aebe11ef59df7c54d2cae1551b
Vikas Singh Varma <vikashverma3001@gmail.com>
2025-06-26 18:41:07 +0530

    Update ChangePassword.jsx

--- frontend/src/pages/password/ChangePassword.jsx
+++ frontend/src/pages/password/ChangePassword.jsx
@@ -3,8 +3,8 @@ import { toast } from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import Layout from "../../Layout/Layout";
-import { changePassword } from "../../Redux/Slices/AuthSlice";
-import InputBox from "../../Components/InputBox/InputBox";
+import { changePassword } from "../../redux/slices/authSlice";
+import InputBox from "../../components/InputBox/InputBox";
 
 export default function ChangePassword() {
   const dispatch = useDispatch();
